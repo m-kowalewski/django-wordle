26e15514a2ec69c276ea7789579d039d21506eda
m-kowalewski <mickow95@gmail.com>
2022-02-07 22:09:58 +0100

    Some work
    Deleting generic.ListView and using request with render.

--- DjangoWordle/WordleColourResult/urls.py
+++ DjangoWordle/WordleColourResult/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name='WordleColourResult'
 urlpatterns=[
 	#ex: /WordleColourResult/
-	path('', views.IndexView.as_view(), name='index')
+	path('', views.index, name='index')
+	#path('', views.IndexView.as_view(), name='index')
 ]
--- DjangoWordle/WordleColourResult/views.py
+++ DjangoWordle/WordleColourResult/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.views import generic
 from django.utils import timezone
+from django.http import HttpResponse, HttpResponseRedirect
 
 from .models import WordleResult
 
-class IndexView(generic.ListView):
-	template_name = 'WordleColourResult/index.html'
-	context_object_name = 'names'
-	
-	def get_queryset(self):
-		return WordleResult.objects.order_by('-result_date')
+#class IndexView(generic.ListView):
+#	template_name = 'WordleColourResult/index.html'
+#	context_object_name = 'names'
+#	
+#	def get_queryset(self):
+#		return WordleResult.objects.order_by('-result_date')
+
+def index(request):
+	ordered_result = WordleResult.objects.order_by('-result_date')
+	return render(request, 'WordleColourResult/index.html', {'names':ordered_result, 'n':range(5)})
