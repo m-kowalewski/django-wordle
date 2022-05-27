fcd0e7882846a8f42493f5fe3c3e70c2fe69d6a9
m-kowalewski <mickow95@gmail.com>
2022-05-27 22:57:25 +0200

    register app created
    Created register app, that contains: register, login. Logout and navigation bar is in progress.

--- DjangoWordle/DjangoWordle/urls.py
+++ DjangoWordle/DjangoWordle/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('WordleColourResult/', include('WordleColourResult.urls')),
+    path('account/', include('register.urls')),
     path('admin/', admin.site.urls),
 ]
